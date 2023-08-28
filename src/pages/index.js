8a0b3e99874568472d564e2351a3bd4d4536824d
Anjela Eshaghzey <anjela-2005@hotmail.com>
2023-08-28 23:29:00 +0200

    space between km and number

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
       {stations.map(station => 
         <Link href={`/stations/${station.id}`} className={`${styles.card} ${station.free_bikes === 0 ? styles.noBikes : (station.empty_slots === 0 ? styles.allBikes : '')}`} key={station.id}> 
           <span className={styles.name}>{station.name}</span>
-          <span className={styles.kilometer}>{getDistance(location.latitude, location.longitude, station.latitude, station.longitude).distance/1000}<span className={styles.km}>km</span></span>
+          <span className={styles.kilometer}>{getDistance(location.latitude, location.longitude, station.latitude, station.longitude).distance/1000}<span className={styles.km}> km</span></span>
           <span className={styles.free}>{station.free_bikes} / {station.empty_slots + station.free_bikes}</span>
         </Link>
       )}
